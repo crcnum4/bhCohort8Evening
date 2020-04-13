d5d1996bf75a21480c4f4dcb2f12ad082c35b9ef
Cliff Choiniere <crcnum4@gmail.com>
2020-04-13 19:17:33 -0400

    created mock resource file and dynimacally load resources to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Resource from "./components/Resource";
+import resources from "./mock/resources";
 
 class App extends Component {
+  //[<Resource resource={aResource}/>, <Resource resource={aResource}]
+  // constructor(props) {
+  //   super(props);
+  //   this.renderPosts.bind(this);
+  // }
+  // Resource = require("./components/Resource");
+
+  renderPosts = () => {
+    const display = resources.map((resource) => {
+      return <Resource resource={resource} />;
+    });
+
+    return display;
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Welcome to BrainHive!</h1>
+        {this.renderPosts()}
       </div>
-    )
+    );
   }
 }
 
 export default App;
-
-
---
+++ src/mock/resources.js
@@ -0,0 +1,64 @@
+const resources = [
+  {
+    posterName: "Joel",
+    resourceAuthor: "Traversy Media",
+    authorSkillLevel: "Advance",
+    cohort: "8",
+    title: "React JS Crash Crourse",
+    categories: ["React"],
+    summary: "Code along about React on youtube",
+    link: "https://www.youtube.com/watch?v=sBws8MSXN7A",
+    resourceType: "Crash Course",
+    datePublished: "2018-09-10.00:00:00.000z",
+    videoLength: null,
+    timeToComplete: 180,
+    rating: 5,
+    comments: [
+      { user: "john", text: "Great video with clear instructions" },
+      { user: "Bella", text: "Simple and straight to the point." },
+    ],
+  },
+  {
+    posterName: "John F",
+    resourceAuthor: "DevEd",
+    authorSkillLevel: "Beginner",
+    cohort: "8",
+    title: "React Tutorial For Beginners",
+    categories: ["React", "Beginner"],
+    summary:
+      "A beginner's guide to React, React State Management, React Router, and Redux. Include 5 app projects.",
+    link:
+      "https://www.youtube.com/playlist?list=PLDyQo7g0_nsVHmyZZpVJyFn5ojlboVEhE",
+    resourceType: "Playlist",
+    datePublished: "2019-06-22.00:00:00.000z",
+    videoLength: null,
+    timeToComplete: 200,
+    rating: 4,
+    comments: [
+      { user: "Tony", text: "That crazy Romanian knows his stuff!" },
+      { user: "Sheila", text: "A good intro to React." },
+    ],
+  },
+  {
+    posterName: "Jaliyah",
+    resourceAuthor: "NetNinja",
+    authorSkillLevel: "Advanced",
+    cohort: "0",
+    title: "React & Redux Complete",
+    categories: ["React", "Redux"],
+    summary:
+      "A good resouce on building a couple react apps with and without redux",
+    link: "www.google.com/search?q=NetNinja%20React",
+    resourceType: "Playlist",
+    datePublished: "2018-09-10.00:00:00.000z",
+    videoLength: null,
+    timeToComplete: 300,
+    rating: 5,
+    comments: [
+      { user: "Joe", text: "Pretty good resource" },
+      { user: "Phen", text: "The best to learn React with." },
+    ],
+  },
+];
+
+export default resources;
