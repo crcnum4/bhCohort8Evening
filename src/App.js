7363bf7d57880c9473eec09cd2c81eb13c0fbb33
Cliff Choiniere <crcnum4@gmail.com>
2020-04-20 19:03:29 -0400

    fill out the form and add functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Resource from "./components/Resource";
 import resources from "./mock/resources";
 
+import ResourceForm from "./components/ResourceForm";
+
 class App extends Component {
   //[<Resource resource={aResource}/>, <Resource resource={aResource}]
   // constructor(props) {
@@ -10,10 +12,20 @@ class App extends Component {
   //   this.renderPosts.bind(this);
   // }
   // Resource = require("./components/Resource");
+  state = {
+    resources: [...resources],
+  };
+
+  addResource = (newResource) => {
+    // this.state.resources.push(newResource);
+    this.setState({
+      resources: [...this.state.resources, newResource],
+    });
+  };
 
   renderPosts = () => {
-    const display = resources.map((resource) => {
-      return <Resource resource={resource} />;
+    const display = this.state.resources.map((resource) => {
+      return <Resource resource={resource} key={resource.title} />;
     });
 
     return display;
@@ -24,8 +36,12 @@ class App extends Component {
       <div className="App">
         <div className="header">
           <h1 className="title">Welcome to BrainHive!</h1>
+          <div className="navigation">
+            <a href="">Add Resource</a>
+          </div>
         </div>
         <div className="resourceList">{this.renderPosts()}</div>
+        <ResourceForm addResource={this.addResource} />
       </div>
     );
   }
--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Resource = ({resource}) => {
-
+const Resource = ({ resource }) => {
   // const {resource} = props;
 
   return (
-    <div className={'box'}>
+    <div className={"box"}>
       <h2>{resource.title}</h2>
-      <p>{resource.author}</p>
+      <p>{resource.resourceAuthor}</p>
       <p>Published on: {resource.datePublished}</p>
-      { resource.videoLength ? (<p>Length: {resource.videoLength} minutes</p>) : null }
+      {resource.videoLength ? (
+        <p>Length: {resource.videoLength} minutes</p>
+      ) : null}
       {/* conditional ? true code : false code */}
       <p>Comments: {resource.comments.length}</p>
     </div>
-  )
-}
+  );
+};
 
 export default Resource;
